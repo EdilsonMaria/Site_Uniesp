73efa6d3446cb8112f2c72855ab9ba24c4096f43
EdilsonMaria <edilson.eng1@gmail.com>
2024-12-02 15:17:22 -0300

    Ajuster do componente Inicial.jsx

--- site_uniesp/src/pages/Inicial.jsx
+++ site_uniesp/src/pages/Inicial.jsx
@@ -9,7 +9,10 @@ const Inicial = () => {
         </Typography>
         <Paper elevation={3} sx={{ p: 3, mb: 4, backgroundColor: '#ffffff', borderRadius: '8px' }}>
             <Typography variant='h5' gutterBottom>
-                Sobre a Faculdade
+                A busca por uma educação de qualidade e pelo desenvolvimento profissional de nossos alunos é a meta que permeia todas as nossas atividades.
+                Colocadas diante de nós desde a fundação, em 8 de Setembro 1998, esses objetivos se refletem em uma estrutura física atraente e completa, 
+                com oito grandes blocos repletos de salas de aulas amplas e climatizadas; auditórios e laboratórios para as discussões práticas da vida acadêmica; 
+                biblioteca com um dos melhores acervos da Paraíba; além de espaço de vivência com praça de alimentação e internet wi-fi.
             </Typography>
             <Typography variant='body1'>
                 A faculdade UNIESP é uma instituição de Ensino Superior...
